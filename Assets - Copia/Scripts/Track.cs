5357b0d1d83210ba1e3b771e0f3d09f8739e5be6
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-10-06 14:46:20 -0300

    Fixed running play

---
+++ Assets - Copia/Scripts/Track.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject wave;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            wave.transform.position = new Vector3(0, -12, transform.position.z);
+            transform.position = new Vector3(0, 0, other.gameObject.transform.position.z + 18 * 4);
+        }
+    }
+}
