dafb922c49ce24ba79826a84009f77770f177256
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-10-06 14:55:16 -0300

    Fixed running play

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,8 +22,6 @@ public class Player : MonoBehaviour
     [SerializeField] private Animator m_animator = null;
     [SerializeField] private Rigidbody m_rigidBody = null;
 
-    [SerializeField] private ControlMode m_controlMode = ControlMode.Direct;
-
     private float m_currentV = 0;
     private float m_currentH = 0;
 
@@ -110,57 +108,16 @@ public class Player : MonoBehaviour
         {
             m_jumpInput = true;
         }
+        m_animator.SetBool("Grounded", m_isGrounded);
+        DirectUpdate();
+        m_wasGrounded = m_isGrounded;
+        m_jumpInput = false;
+        m_moveSpeed += 1 * Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
-        m_animator.SetBool("Grounded", m_isGrounded);
-
-        switch (m_controlMode)
-        {
-            case ControlMode.Direct:
-                DirectUpdate();
-                break;
-
-            case ControlMode.Tank:
-                TankUpdate();
-                break;
-
-            default:
-                Debug.LogError("Unsupported state");
-                break;
-        }
-
-        m_wasGrounded = m_isGrounded;
-        m_jumpInput = false;
-    }
-
-    private void TankUpdate()
-    {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
-        bool walk = Input.GetKey(KeyCode.LeftShift);
-
-        if (v < 0)
-        {
-            if (walk) { v *= m_backwardsWalkScale; }
-            else { v *= m_backwardRunScale; }
-        }
-        else if (walk)
-        {
-            v *= m_walkScale;
-        }
-
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
-        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
-
-        m_animator.SetFloat("MoveSpeed", m_currentV);
-
-        JumpingAndLanding();
+       
     }
 
     private void DirectUpdate()
@@ -169,7 +126,6 @@ public class Player : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
 
         Transform camera = Camera.main.transform;
-
         if (Input.GetKey(KeyCode.LeftShift))
         {
             v *= m_walkScale;
@@ -184,12 +140,10 @@ public class Player : MonoBehaviour
         float directionLength = direction.magnitude;
         direction.y = 0;
         direction = direction.normalized * directionLength;
-
         if (direction != Vector3.zero)
         {
             m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
 
-            transform.rotation = Quaternion.LookRotation(m_currentDirection);
             transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
 
             m_animator.SetFloat("MoveSpeed", direction.magnitude);
--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -6,6 +6,7 @@ public class Track : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject wave;
+    public GameObject road;
     void Start()
     {
         
@@ -14,15 +15,16 @@ public class Track : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            wave.transform.position = new Vector3(0, -12, transform.position.z);
-            transform.position = new Vector3(0, 0, other.gameObject.transform.position.z + 18 * 4);
+            wave.transform.position = new Vector3(0, -12, wave.transform.position.z + 15);
+            road.transform.position = new Vector3(0, 0, road.transform.position.z + 15 * 5);
         }
     }
+
 }
