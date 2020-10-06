bbb9585bd57720e37e8d4316b6b45311cbe764df
Matheus Nascimento <matheunascimentoti99@gmail.com>
2020-10-06 10:26:50 -0300

    Upload project

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GamePlay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        
+    }
+}
